275e7131a5f25311b38413c20c596b7638909b28
Alex Peters <alpe@users.noreply.github.com>
2024-07-17 13:06:18 +0200

    Start app v2 support for sims2

--- simapp/v2/app_di.go
+++ simapp/v2/app_di.go
@@ -14,6 +14,7 @@ import (
 	"cosmossdk.io/runtime/v2"
 	"cosmossdk.io/store/v2/root"
 	upgradekeeper "cosmossdk.io/x/upgrade/keeper"
+	_ "github.com/cosmos/cosmos-sdk/x/genutil"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
